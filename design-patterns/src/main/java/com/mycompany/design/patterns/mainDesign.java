c6464cb752b8f552c0ae08b8042d0f7c0ab09b72
Juan Ignacio Caballero <juan.ci.caballero@gmail.com>
2021-11-29 14:21:18 -0300

    Main created

---
+++ design-patterns/src/main/java/com/mycompany/design/patterns/mainDesign.java
@@ -0,0 +1,9 @@
+package com.mycompany.design.patterns;
+
+public class mainDesign {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hello to Desing Patterns");
+    }
+}
